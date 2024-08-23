769e93506a48ad9b00a1ce5b08b0be402cce6de0
trungsliver <buiductrung.cva@gmail.com>
2024-08-23 18:14:37 +0700

    test

--- test.py
+++ test.py
@@ -1,1 +1,34 @@
-print('hello')
+# print('Duc Trung')
+
+# # Các kiểu dữ liệu
+#     # Int - số nguyên
+# grade = 6
+#     # Float - số thực
+# pi = 3.14
+#     # String - chuỗi ký tự
+# name = 'Duc Trung'
+#     # Boolean/Bool - True/False
+# male = True
+
+# print('Lớp:', grade)
+# print(grade, pi, name, male)
+
+# game = input('Nhập tựa game bạn thích: ')
+# print('Tựa game bạn thích:', game)
+
+# age = int(input('Nhập tuổi của bạn: '))
+# print('Tuổi:', age)
+
+# Các phép toán
+    # Cơ bản: + - * /
+    # Chia lấy dư: %
+    # Chia lấy nguyên: //
+# print(7/3)
+# print(7%3)
+# print(7//3) 
+
+number = int(input('Nhập 1 số nguyên: '))
+if number % 2 == 0:
+    print(number, 'là số chẵn')
+else:
+    print(number, 'là số lẻ')
