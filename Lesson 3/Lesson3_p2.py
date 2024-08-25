8303d568e1c8817b7bdbbbd00b0b6c04f1647703
trungsliver <buiductrung.cva@gmail.com>
2024-08-25 16:01:20 +0700

    Lesson 3 update

--- Lesson 3/Lesson3_p2.py
+++ Lesson 3/Lesson3_p2.py
@@ -1,11 +1,22 @@
 from Lesson3_p1 import Human, Animal
 from Lesson3_p1 import Student, Dog
 
-
 # stu1 = Student('Khang Anh', 12, 'rainbow', 'FPT School')
 # stu1.introduce()
 # stu2 = Student('Tu', 13, 'male', 'Vin')
 # stu2.introduce()
+
+# Đề bài:
+# Tạo class Animal gồm các thuộc tính: tên, loài
+# Viết 2 phương thức cho class Animal
+
+# Tạo class Dog kế thừa từ class Animal và có thêm thuộc tính: giống
+# Viết 1 phương thức kế thừa từ class Animal (có sửa đổi)
+# Viết 1 phương thức mới cho class Dog
+
+# Yêu cầu:
+# - Tạo class ở file Lesson3_p1
+# - Viết chương trình test tại file Lesson_3_p2
 
 dog1 = Dog('Nick', 'Chó', 'Shiba')
 dog1.sleep()
