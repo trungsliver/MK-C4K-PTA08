6bbed3f98187baf602222d5154972edd81b45b65
trungsliver <buiductrung.cva@gmail.com>
2024-09-28 09:24:00 +0700

    sign up

--- Lesson 7/main.py
+++ Lesson 7/main.py
@@ -9,6 +9,7 @@ import login, signup
 ui = ''
 app = QtWidgets.QApplication(sys.argv) 
 MainWindow = QtWidgets.QMainWindow()
+users = ['admin:admin']
 
 def homeUi():
     global ui
@@ -17,8 +18,57 @@ def homeUi():
     ui.btn_signup.clicked.connect(signup_check)
     MainWindow.show()
 
+def login_w():
+    global ui
+    ui = login.Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    # ui.btn_signup.clicked.connect(signup_check)
+    MainWindow.show()
+
 def signup_check():
-    print('signup')
+    check = True
+    name = ui.lineEdit_name.text().strip()
+    username = ui.lineEdit_email.text().strip()
+    password = ui.lineEdit_password.text().strip()
+    confirm = ui.lineEdit_confirm.text().strip()
+    # Thiếu thông tin
+    if name == '' or username == '' or password == '' or confirm == '':
+        check = False
+        msg_box('Signup fail', 'Missing information!')
+    # Pass và confirm không khớp
+    elif password != confirm:
+        check = False
+        msg_box('Signup fail', 'Password and confirm password are not match!')
+    # Đã tồn tại username trong danh sách users
+    else:
+        for user in users:
+            stored_username, stored_password = user.split(':', 1)
+            if username == stored_username:
+                check = False
+                msg_box('Signup fail', 'Username already exists!')
+    
+    # Kiểm tra xem có đang ký thành công không
+    if check == True:
+        # Add tài khoản mới vào danh sách users
+        new_acc = username + ':' + password
+        users.append(new_acc)
+        msg_box('Signup success', 'Signup success!')
+        print(users)
+        login_w()
+
+
+
+def msg_box(title, content):
+    msg = QtWidgets.QMessageBox()
+    msg.setStyleSheet("QLabel{min-width: 200px;}"
+                          "QLabel{max-width: 200px;}"
+                          "QMessageBox{background-color:rgba(35,36,40,255);}"
+                          "QPushButton{background-color:rgb(30,95,181);}"
+                          "QLabel{color:rgb(255,255,255);}"
+                          "QPushButton{color:rgb(255,255,255);}")
+    msg.setWindowTitle(title)
+    msg.setInformativeText(content)
+    msg.exec()
 
 #Run app
 homeUi()
